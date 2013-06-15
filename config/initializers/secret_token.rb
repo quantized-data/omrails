e3011d75eb1d73d116d81ce710066c5d0a7cc0c0
data ng <data@9lab.co>
2013-06-16 01:20:57 +0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Omrails::Application.config.secret_token = '8784cbe1a6ad6f323212974956e9f331723b68df070bf51089479611a236ce3c0b1bdcb465e131404cecfe1a0ff1d48b8b91ad4bfca52a97da8bb8ed53f1b99e'
